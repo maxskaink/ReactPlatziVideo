3384ec1fe50d82caeac1e10e65b09a03b90580af
maxskaink <calambasvivas11@gmail.com>
2020-07-17 15:38:02 -0500

    Se han corregido unos problemas segun eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,54 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 /* const MiniCssExtractPlugin = require("mini-css-extract-plugin"); */
 
 module.exports = {
-    entry: "./src/index.js",//Entrada y inicio
-    output: {
-        path: path.resolve(__dirname, "dist"),//Se necesita a path
-        filename: "bundle.js"
-    },//Salida cuando se termina la compilacion
-    resolve: {
-        extensions: [".js", ".jsx"]
-    },//Resuleve las extenciones que se van a utilizar en el pryecto
-    module: {
-        rules: [{
-            test: /\.(js|jsx)$/, //indetificas los archivos con las extenciones escritas
-            exclude: /node_modules/, //Excluye a node_modules
-            use: {
-                loader: "babel-loader"//Se declara que se va a utilizar babel
-            }
-        },
+  entry: './src/index.js', //Entrada y inicio
+  output: {
+    path: path.resolve(__dirname, 'dist'), //Se necesita a path
+    filename: 'bundle.js',
+  }, //Salida cuando se termina la compilacion
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  }, //Resuleve las extenciones que se van a utilizar en el pryecto
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/, //indetificas los archivos con las extenciones escritas
+      exclude: /node_modules/, //Excluye a node_modules
+      use: {
+        loader: 'babel-loader', //Se declara que se va a utilizar babel
+      },
+    },
+    {
+      test: /\.html$/, //declara que se va a utlizar html
+      use: [{
+        loader: 'html-loader', //usar loader
+      },
+      ],
+    },
+    {
+      test: /\.(s*)css$/,
+      use: [
         {
-            test: /\.html$/, //declara que se va a utlizar html
-            use: [{
-                loader: "html-loader" //usar loader
-            }
-        ]
+          loader: MiniCssExtractPlugin.loader,
         },
-        {
-            test: /\.(s*)css$/,
-            use: [
-                {
-                    loader: MiniCssExtractPlugin.loader
-                },
-                "css-loader",
-                "sass-loader"
-            ]
-        }
-    ]
-    },//Se crean las reglas
+        'css-loader',
+        'sass-loader',
+      ],
+    },
+    ],
+  }, //Se crean las reglas
 
-    plugins:[
-        new HtmlWebPackPlugin({
-            template: './public/index.html',
-            filename: './index.html'
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
 
-        }),
-        new MiniCssExtractPlugin({
-            filename:"assets/[name].css"
-        })
-    ]//Se declaran los plugins
-    
-}
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    }),
+  ], //Se declaran los plugins
+
+};
